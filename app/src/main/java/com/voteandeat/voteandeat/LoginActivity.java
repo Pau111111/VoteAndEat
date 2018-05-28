4cd6b7c41cdf13fd61e336fef2fda02b9467eeb7
Joan <boschruzjoan@gmail.com>
2018-05-28 16:35:23 +0200

    Login with Google corrected

--- app/src/main/java/com/voteandeat/voteandeat/LoginActivity.java
+++ app/src/main/java/com/voteandeat/voteandeat/LoginActivity.java
@@ -26,7 +26,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.voteandeat.voteandeat.Model.User;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
@@ -37,6 +41,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     //SignInButton signInButton;
     Button btnRegister, btnLogin;
     ProgressBar progressBarLogin;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,20 +91,46 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            String user = mAuth.getCurrentUser().getDisplayName();
-                            String email = mAuth.getCurrentUser().getEmail();
-                            String url = mAuth.getCurrentUser().getPhotoUrl().toString();
-                            User userAux = new User(
-                                    user,email
+                            final String email = mAuth.getCurrentUser().getEmail();
+                            mDatabase = FirebaseDatabase.getInstance().getReference();
+                            mDatabase.child("Users").orderByChild("email").equalTo(email).addListenerForSingleValueEvent(
+                                    new ValueEventListener() {
+
+                                        @Override
+                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                            if (dataSnapshot.exists()) {
+                                                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                                                startActivity(i);
+                                                finish();
+                                                // User Exists
+                                                // Do your stuff here if user already exists
+                                            } else {
+
+                                                // User Not Yet Exists
+                                                // Do your stuff here if user not yet exists
+                                                String user = mAuth.getCurrentUser().getDisplayName();
+                                                String url = mAuth.getCurrentUser().getPhotoUrl().toString();
+                                                User userAux = new User(
+                                                        user,email
+                                                );
+                                                userAux.setPhotourl(url);
+                                                FirebaseDatabase.getInstance().getReference("Users")
+                                                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                                        .setValue(userAux);
+                                                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                                                startActivity(i);
+                                                finish();
+                                                Toast.makeText(getApplicationContext(), "User logged succesfully", Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError databaseError) {
+
+                                        }
+                                    }
+
                             );
-                            userAux.setPhotourl(url);
-                            FirebaseDatabase.getInstance().getReference("Users")
-                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                    .setValue(userAux);
-                            Intent i = new Intent(getApplicationContext(), HomeActivity.class);
-                            startActivity(i);
-                            finish();
-                            Toast.makeText(getApplicationContext(), "User logged succesfully", Toast.LENGTH_LONG).show();
                         } else {
                             // If sign in fails, display a message to the user.
                             Toast.makeText(getApplicationContext(), "Could not log in user", Toast.LENGTH_LONG).show();
