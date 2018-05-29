df1b2646479842f26e8bff26fc7cafd6287701d7
Pau111111 <Cacatua111111>
2018-05-29 18:30:10 +0200

    Solved Firebase error version

--- app/src/main/java/com/voteandeat/voteandeat/MainActivity.java
+++ app/src/main/java/com/voteandeat/voteandeat/MainActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 2000;
