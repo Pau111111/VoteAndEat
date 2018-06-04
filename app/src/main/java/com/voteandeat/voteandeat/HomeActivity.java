284425e5fb2310cc69d14abb3243ea90fb805c31
Pau111111 <Cacatua111111>
2018-06-04 21:49:50 +0200

    All activities will display in portrait and new welcome page added

--- app/src/main/java/com/voteandeat/voteandeat/HomeActivity.java
+++ app/src/main/java/com/voteandeat/voteandeat/HomeActivity.java
@@ -137,9 +137,6 @@ public class HomeActivity extends AppCompatActivity
                 username = dataSnapshot.child(id).child("name").getValue(String.class);
                 nameUser.setText(username);
 
-                TextView textUser = findViewById(R.id.textUserLogged);
-                textUser.setText(username);
-
                 ImageView imageUser = headerView.findViewById(R.id.imageUser);
                 urlUser = dataSnapshot.child(id).child("photourl").getValue(String.class);
                 Picasso.get().load(urlUser).into(imageUser);
